f49e5c6ea6dc4684a8d208a79212c722168df9dc
hamzakamelen <hamzakamelen12345@gmail.com>
2025-06-15 02:55:35 +0500

    Update

--- module/vectorstore.py
+++ module/vectorstore.py
@@ -7,49 +7,24 @@ import os
 
 PERSIST_DIR = "./chroma_store"
 
-# def create_vector_store(uploaded_files):
-#     paths = save_uploaded_file(uploaded_files)
-#     docs = []
-#     for paths in paths:
-#         loader = PyPDFLoader(paths)
-#         docs.extend(loader.load())
-    
-#     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-#     text = text_splitter.split_documents(docs)
-    
-#     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L12-v2")
-#     # embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
-    
-#     # if os.path.exists(PERSIST_DIR) and os.listdir(PERSIST_DIR):
-#     vector_store = Chroma(persist_directory=PERSIST_DIR, embedding_function=embeddings)
-#     vector_store.add_documents(text)
-#     vector_store.persist()
-#     # else:
-#     #     pass
-#         # vector_store = Chroma.from_documents(text, embeddings, persist_directory=PERSIST_DIR)
-#         # vector_store.persist()
-#     return vector_store 
-
-
 def create_vector_store(uploaded_files):
-    if os.path.exists(PERSIST_DIR) and os.listdir(PERSIST_DIR):
-        # Use prebuilt vector store
-        embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L12-v2")
-        vector_store = Chroma(persist_directory=PERSIST_DIR, embedding_function=embeddings)
-        return vector_store
-
-    # Else, build new vector store
     paths = save_uploaded_file(uploaded_files)
     docs = []
-    for path in paths:
-        loader = PyPDFLoader(path)
+    for paths in paths:
+        loader = PyPDFLoader(paths)
         docs.extend(loader.load())
     
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     text = text_splitter.split_documents(docs)
     
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L12-v2")
-    vector_store = Chroma.from_documents(text, embeddings, persist_directory=PERSIST_DIR)
-    vector_store.persist()
+    # embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
     
-    return vector_store
+    if os.path.exists(PERSIST_DIR) and os.listdir(PERSIST_DIR):
+        vector_store = Chroma(persist_directory=PERSIST_DIR, embedding_function=embeddings)
+        vector_store.add_documents(text)
+        vector_store.persist()
+    else:
+        vector_store = Chroma.from_documents(text, embeddings, persist_directory=PERSIST_DIR)
+        vector_store.persist()
+    return vector_store 
